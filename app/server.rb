ed12c942b1aae2d985b47cf98bbb053b9115c21f
Rogan <rogan.berry@splitpoint.co.za>
2024-06-03 15:10:13 +0200

    third section

--- app/server.rb
+++ app/server.rb
@@ -7,7 +7,7 @@ print("Logs from your program will appear here!")
 #
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
-client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+#client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
 
 if client_socket.gets = "GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n"
 client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
