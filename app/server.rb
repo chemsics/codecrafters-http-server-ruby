a90e17e95a977d8b9462eb5b02fdaa9c421ce829
Rogan <rogan.berry@splitpoint.co.za>
2024-06-03 15:23:39 +0200

    third section

--- app/server.rb
+++ app/server.rb
@@ -6,11 +6,16 @@ print("Logs from your program will appear here!")
 # Uncomment this to pass the first stage
 #
 server = TCPServer.new("localhost", 4221)
-client_socket, client_address = server.accept
+#client_socket, client_address = server.accept
 #client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
 
-if client_socket.gets "GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n"
-client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
-else 
+loop do
+  client_socket, client_address = server.accept
+  if request = "/"
+  client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+  else
   client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+  end 
+  client.close
 end
+
